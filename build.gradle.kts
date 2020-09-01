cb1ec412e57517e371fce47ac9f875798fa99ca2
bjongbloedt <bjongbloedt@users.noreply.github.com>
2020-09-01 15:15:18 -0500

    adding dev tools for hot reload

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
 //    implementation("org.flywaydb:flyway-core")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
 //    runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
